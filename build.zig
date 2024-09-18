e857b9fb876d2d7affa05abdc5b8f6b26e7fe986
nk521 <nk_mason@protonmail.com>
2024-09-19 00:03:27 +0530

    chore: fix paths

--- build.zig
+++ build.zig
@@ -1,20 +1,20 @@
 const std = @import("std");
 const zlib = @import("zlib.zig");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     // Modules available to downstream dependencies
     _ = b.addModule("zlib", .{
-        .source_file = .{ .path = (comptime thisDir()) ++ "/src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
     });
 
     const lib = zlib.create(b, target, optimize);
     b.installArtifact(lib.step);
 
     const tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
     });
     lib.link(tests, .{});
 
@@ -23,15 +23,10 @@ pub fn build(b: *std.build.Builder) void {
 
     const bin = b.addExecutable(.{
         .name = "example1",
-        .root_source_file = .{ .path = "example/example1.zig" },
+        .root_source_file = b.path("example/example1.zig"),
         .target = target,
         .optimize = optimize,
     });
     lib.link(bin, .{ .import_name = "zlib" });
     b.installArtifact(bin);
-}
-
-/// Path to the directory with the build.zig.
-fn thisDir() []const u8 {
-    return std.fs.path.dirname(@src().file) orelse unreachable;
 }
--- zlib.zig
+++ zlib.zig
@@ -1,33 +1,27 @@
 const std = @import("std");
 const Self = @This();
 
-fn root() []const u8 {
-    return std.fs.path.dirname(@src().file) orelse ".";
-}
-
-const root_path = root() ++ "/";
-const package_path = root_path ++ "src/main.zig";
-pub const include_dir = root_path ++ "zlib";
 pub const Options = struct {
     import_name: ?[]const u8 = null,
 };
 
 pub const Library = struct {
-    step: *std.build.LibExeObjStep,
+    b: *std.Build,
+    step: *std.Build.Step.Compile,
 
-    pub fn link(self: Library, other: *std.build.LibExeObjStep, opts: Options) void {
-        other.addIncludePath(.{ .path = include_dir });
+    pub fn link(self: Library, other: *std.Build.Step.Compile, opts: Options) void {
+        other.addIncludePath(self.b.path("zlib"));
         other.linkLibrary(self.step);
 
         if (opts.import_name) |import_name|
-            other.addAnonymousModule(
+            other.root_module.addAnonymousImport(
                 import_name,
-                .{ .source_file = .{ .path = package_path } },
+                .{ .root_source_file = self.b.path("src/main.zig") },
             );
     }
 };
 
-pub fn create(b: *std.build.Builder, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) Library {
+pub fn create(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) Library {
     const ret = b.addStaticLibrary(.{
         .name = "z",
         .target = target,
@@ -36,23 +30,23 @@ pub fn create(b: *std.build.Builder, target: std.zig.CrossTarget, optimize: std.
     ret.linkLibC();
     ret.addCSourceFiles(.{ .files = srcs, .flags = &.{"-std=c89"} });
 
-    return Library{ .step = ret };
+    return Library{ .b = b, .step = ret };
 }
 
 const srcs = &.{
-    root_path ++ "zlib/adler32.c",
-    root_path ++ "zlib/compress.c",
-    root_path ++ "zlib/crc32.c",
-    root_path ++ "zlib/deflate.c",
-    root_path ++ "zlib/gzclose.c",
-    root_path ++ "zlib/gzlib.c",
-    root_path ++ "zlib/gzread.c",
-    root_path ++ "zlib/gzwrite.c",
-    root_path ++ "zlib/inflate.c",
-    root_path ++ "zlib/infback.c",
-    root_path ++ "zlib/inftrees.c",
-    root_path ++ "zlib/inffast.c",
-    root_path ++ "zlib/trees.c",
-    root_path ++ "zlib/uncompr.c",
-    root_path ++ "zlib/zutil.c",
+    "zlib/adler32.c",
+    "zlib/compress.c",
+    "zlib/crc32.c",
+    "zlib/deflate.c",
+    "zlib/gzclose.c",
+    "zlib/gzlib.c",
+    "zlib/gzread.c",
+    "zlib/gzwrite.c",
+    "zlib/inflate.c",
+    "zlib/infback.c",
+    "zlib/inftrees.c",
+    "zlib/inffast.c",
+    "zlib/trees.c",
+    "zlib/uncompr.c",
+    "zlib/zutil.c",
 };
